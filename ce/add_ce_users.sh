2dcf7bb0dd1f1d7bb2fea4adc5bc8952a6ef994a
Igor Sfiligoi <isfiligoi@ucsd.edu>
2019-09-05 14:29:35 -0700

    Add support for cms

--- ce/add_ce_users.sh
+++ ce/add_ce_users.sh
@@ -9,4 +9,5 @@ groupadd -g 3002 icecube && useradd -u 3002 -g 3002 -s /bin/bash icecube
 groupadd -g 3003 glow && useradd -u 3003 -g 3003 -s /bin/bash glow
 groupadd -g 3004 ligo && useradd -u 3004 -g 3004 -s /bin/bash ligo
 groupadd -g 3005 hcc && useradd -u 3005 -g 3005 -s /bin/bash hcc
+groupadd -g 3006 cms && useradd -u 3006 -g 3006 -s /bin/bash cms
 
